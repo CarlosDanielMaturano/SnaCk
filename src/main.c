2403b2677e1e40ce171e323fc5819fc0981c8a16
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-08-03 08:35:57 -0300

    feat: :sparkles: Add clear_screen function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+void clear_screen();
+
 int main(void) {
-    printf("Hello, World\n");
+    for (;;)
+        clear_screen();
     return 0;
+}
+
+void clear_screen() {
+    printf("\e[1;1H\e[2J");
 }
