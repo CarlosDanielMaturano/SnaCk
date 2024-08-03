171c9129ede943ad5e44829bbe8681b80a4669d6
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-08-03 08:47:59 -0300

    feat: :sparkles: Add map drawing function

--- src/main.c
+++ src/main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
+#define MAP_WIDTH 30
+#define MAP_HEIGHT 15
+#define SLEEP_TIME 160000 // 16 miliseconds in microseconds
 
 void clear_screen();
+void draw_map(int [MAP_HEIGHT][MAP_WIDTH]);
+
+int map[MAP_HEIGHT][MAP_WIDTH];
 
 int main(void) {
-    for (;;)
+    for (;;) {
         clear_screen();
+        draw_map(map);
+        usleep(SLEEP_TIME);
+    }
     return 0;
 }
 
 void clear_screen() {
     printf("\e[1;1H\e[2J");
+}
+
+void draw_map(int map[MAP_HEIGHT][MAP_WIDTH]) {
+    for (size_t j = 0; j < MAP_HEIGHT; j++)  {
+        for (size_t i = 0; i < MAP_WIDTH; i++) 
+            putchar('+');
+        putchar('\n');
+    }
 }
